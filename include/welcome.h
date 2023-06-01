b3bc751557619ce379c1965c890ddc98d497ac69
breadteeth <zhangxx5@shanghaitech.edu.cn>
2023-06-01 20:47:17 +0800

    encapsulation

--- include/welcome.h
+++ include/welcome.h
@@ -1,10 +1,12 @@
 #ifndef WELCOME_H
 #define WELCOME_H
 #include "game.h"
+#include "lcd/lcd.h"
 
 void draw_welcome();
 void draw_select(int level, int box_num);
 void select_stage(int *level, int *box_num);
 int End_stage(Game* game, int scoreboard[3][4]);
-
+void draw_snowflake(u16 x,u16 y,u16 r);
+void animation();
 #endif
--- src/main.c
+++ src/main.c
@@ -29,84 +29,11 @@ void update_scoreboard(int scoreboard[3][4], Game* game){
     qsort(scoreboard[level-1],4,sizeof(int),comp);
 }
 
-void draw_snowflake(u16 x,u16 y,u16 r){
-    int i=0;
-    for(i=y-r;i<=y+r;i++){
-        LCD_DrawPoint(x,i,BLUE);
-    }
-    for(i=x-r;i<=x+r;i++){
-        LCD_DrawPoint(i,y,BLUE);
-    }
-    for(i=0;i<2*r;i++){
-        LCD_DrawPoint(x-(r-1)+i,y+(r-1)-i,BLUE);
-    }
-    for(i=0;i<2*r;i++){
-        LCD_DrawPoint(x-(r-1)+i,y-(r-1)+i,BLUE);
-    }
-}
-void animation(){
-    LCD_Clear(BLACK);
-    int start_time = 0;
-    //LCD_ShowString(60,25,"Mygame",WHITE);
-    int rx,ry,r;
-    rx = 10;ry = 10;r=80;
-    int dx = 0;int dy=0;
-    while(dx<LCD_W){
-        LCD_DrawCircle(dx,LCD_H/2,10,BLUE);
-        delay_1ms(10);
-        dx+=1;
-    }
-    while(dy<LCD_H){
-        LCD_DrawCircle(LCD_W/2,dy,10,BLUE);
-        delay_1ms(10);
-        dy+=1;
-    }
-    int rr=0;
-    while(rr<LCD_W/2){
-        LCD_DrawCircle(LCD_W/2,LCD_H/2,rr,BLACK);
-        delay_1ms(20);
-        rr+=1;
-    }
 
-    LCD_Clear(BLACK);
-    dy=20;
-
-    while(dy<60){
-        LCD_ShowString(36,50,"Loading ...",RED);
-        draw_snowflake(20,dy,6);
-        draw_snowflake(50,dy-12,8);
-        draw_snowflake(80,dy+20,8);
-        draw_snowflake(102,dy+8,6);
-        draw_snowflake(120,dy-7,4);
-        draw_snowflake(70,dy-2,6);
-        delay_1ms(10);
-        LCD_Clear(BLACK);
-        dy++;
-    }
-    while(r>0){
-        LCD_ShowString(30,25,"Welcome to ...",WHITE);
-        LCD_DrawCircle(LCD_W/2,LCD_H/2,r,BLUE);
-        LCD_DrawRectangle(LCD_W/2-60, LCD_H/2-20, LCD_W/2+60, LCD_H/2+20,RED);
-        delay_1ms(20);
-        LCD_Clear(BLACK);
-        r-=2;
-    }
-    while(r<80){
-        LCD_ShowString(40,25,"Our game!",WHITE);
-        LCD_DrawCircle(LCD_W/2,LCD_H/2,r,BLUE);
-        LCD_DrawRectangle(LCD_W/2-60, LCD_H/2-20, LCD_W/2+60, LCD_H/2+20,RED);
-        delay_1ms(20);
-        LCD_Clear(BLACK);
-        r+=2;
-    }
-
-    LCD_Clear(BLACK);
-
-}
 int main(void) {
     IO_init();         // init OLED
 
-/*TODO：display a smooth animation along with the game's title.*/
+/*display a smooth animation along with the game's title.*/
     animation();
 
 /*This includes:initialize the newly created game's data/input + choose level and set the number of boxes*/
